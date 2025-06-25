0840ed65b966a731c47638fc0c53d27a9ccbbcf5
Ahmed baraa <ahmedbaraa009@gmail.com>
2025-06-25 10:58:29 +0300

    updates

--- src/events/ticket-updated-event.ts
+++ src/events/ticket-updated-event.ts
@@ -1,7 +1,7 @@
 import { Subjects } from './subjects';
 
 // to make sure the class consistent subject and data related to that subject
-export interface TicketupdatedEvent {
+export interface TicketUpdatedEvent {
   subject: Subjects.TicketCreated;
   data: {
     id: string;
